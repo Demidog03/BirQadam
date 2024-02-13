a799cc318317093542792454b41412bc39cde54e
DESKTOP-MTGDTDN\Aleksey <a.n.makarenka@gmail.com>
2024-02-13 17:02:51 +0300

    feat: add burger menu, add help icon refactor: menu

--- src/modules/navbar/ui/Navbar.tsx
+++ src/modules/navbar/ui/Navbar.tsx
@@ -1,15 +1,13 @@
-import { Button } from '@/shared/shadcnUI/button';
 import { FC } from 'react';
+import BurgerMenu from './BurgerMenu';
+import Menu from './Menu';
 
 const Navbar: FC = () => {
   return (
     <div className="w-full bg-white px-[50px] py-4 flex justify-between items-center">
       <h1 className="text-lg text-[#0d141c] font-bold">OneStep</h1>
-      <div className="flex items-center gap-2.5">
-        <Button variant="blue" type="button">
-          Sign in
-        </Button>
-      </div>
+      <Menu />
+      <BurgerMenu />
     </div>
   );
 };
