8d3c5991bf49b857a04cee312c23e67b547cb4db
DESKTOP-MTGDTDN\Aleksey <a.n.makarenka@gmail.com>
2024-02-17 14:18:52 +0300

    Revert "fix: fix mistakes"
    This reverts commit 52527a9a7578efc2927ab6651c0f5d210b37bdd0.

--- src/modules/guards/AuthPageGuard.tsx
+++ src/modules/guards/AuthPageGuard.tsx
@@ -1,27 +1,26 @@
-import { type FC, type PropsWithChildren, useState } from 'react';
-import { useLocation, Navigate } from 'react-router-dom';
+import { type FC, type PropsWithChildren, useState } from 'react'
+import { useLocation, Navigate } from 'react-router'
 import AuthPage from '@/pages/AuthPage.tsx';
 import { profileSelector } from '@/modules/profile/model/profile.slice.ts';
 import { useSelector } from '@/store';
 
+
 export const AuthPageGuard: FC<PropsWithChildren> = ({ children }) => {
-  const profile = useSelector(profileSelector);
-  const location = useLocation();
-  const [requestedLocation, setRequestedLocation] = useState<string | null>(
-    null
-  );
+  const profile = useSelector(profileSelector)
+  const location = useLocation()
+  const [requestedLocation, setRequestedLocation] = useState<string | null>(null)
 
   if (!profile) {
     if (location.pathname !== requestedLocation) {
-      setRequestedLocation(location.pathname);
+      setRequestedLocation(location.pathname)
     }
-    return <AuthPage />;
+    return <AuthPage/>
   }
 
   if (requestedLocation && location.pathname !== requestedLocation) {
-    setRequestedLocation(null);
-    return <Navigate to={requestedLocation} />;
+    setRequestedLocation(null)
+    return <Navigate to={requestedLocation} />
   }
 
-  return <>{children}</>;
-};
+  return <>{children}</>
+}
--- src/modules/sidebar/ui/Sidebar.tsx
+++ src/modules/sidebar/ui/Sidebar.tsx
@@ -1,44 +1,40 @@
 import { memo, useEffect, useRef, useState } from 'react';
 import { BiSolidBriefcase, BiSolidHomeAlt2, BiSolidUser } from 'react-icons/bi';
 import usePathnameSegments from '@/shared/lib/hooks/usePathnameSegments.tsx';
-import SidebarMenuItem, {
-  TSidebarMenuItem,
-} from '@/modules/sidebar/ui/SidebarMenuItem.tsx';
-import { useDispatch } from 'react-redux';
-import { setSidebarWidth } from '@/modules/sidebar/model/sidebar.slice.ts';
+import SidebarMenuItem, { TSidebarMenuItem } from '@/modules/sidebar/ui/SidebarMenuItem.tsx';
+import {useDispatch} from "react-redux";
+import {setSidebarWidth} from "@/modules/sidebar/model/sidebar.slice.ts";
 
 const initialSidebarMenuItems: TSidebarMenuItem[] = [
   {
-    icon: <BiSolidHomeAlt2 className="w-5 h-5" />,
+    icon: <BiSolidHomeAlt2 className="w-5 h-5"/>,
     isActive: false,
     text: 'Home',
-    routeName: 'home',
+    routeName: 'home'
   },
   {
-    icon: <BiSolidBriefcase className="w-5 h-5" />,
+    icon: <BiSolidBriefcase className="w-5 h-5"/>,
     isActive: false,
     text: 'CV',
-    routeName: 'cv',
+    routeName: 'cv'
   },
   {
-    icon: <BiSolidUser className="w-5 h-5" />,
+    icon: <BiSolidUser className="w-5 h-5"/>,
     isActive: false,
     text: 'Profile',
-    routeName: 'api',
+    routeName: 'api'
   },
 ];
 
 const Sidebar = memo(() => {
   {
-    const dispatch = useDispatch();
-    const sidebarRef = useRef<HTMLDivElement | null>(null);
+    const dispatch = useDispatch()
+    const sidebarRef = useRef<HTMLDivElement | null>(null)
     const pathnameSegments = usePathnameSegments();
-    const [sidebarMenuItems, setSidebarMenuItems] = useState<
-      TSidebarMenuItem[]
-    >(initialSidebarMenuItems);
+    const [sidebarMenuItems, setSidebarMenuItems] = useState<TSidebarMenuItem[]>(initialSidebarMenuItems);
 
     useEffect(() => {
-      const updatedItems = initialSidebarMenuItems.map((item) => ({
+      const updatedItems = initialSidebarMenuItems.map(item => ({
         ...item,
         isActive: pathnameSegments[0] === item.routeName,
       }));
@@ -47,18 +43,13 @@ const Sidebar = memo(() => {
 
     useEffect(() => {
       if (sidebarRef.current?.clientWidth) {
-        dispatch(setSidebarWidth(sidebarRef.current.clientWidth));
+        dispatch(setSidebarWidth(sidebarRef.current.clientWidth))
       }
     }, [dispatch, sidebarRef]);
 
     return (
-      <div
-        ref={sidebarRef}
-        className="flex flex-col pl-2 pr-4 pt-[30px] pb-[34px] gap-4 border-r border-[#F7F9FC] shadow-[3px_0px_40px_0px_rgba(87,156,216,0.10)] z-10"
-      >
-        <h1 className="text-2xl text-sky-700 font-semibold ml-4 mb-4">
-          OneStep
-        </h1>
+      <div ref={sidebarRef} className="flex flex-col pl-2 pr-4 pt-[30px] pb-[34px] gap-4 border-r border-[#F7F9FC] shadow-[3px_0px_40px_0px_rgba(87,156,216,0.10)] z-10">
+        <h1 className="text-2xl text-sky-700 font-semibold ml-4 mb-4">OneStep</h1>
         {sidebarMenuItems.map((item) => (
           <SidebarMenuItem
             key={item.routeName}
@@ -71,6 +62,6 @@ const Sidebar = memo(() => {
       </div>
     );
   }
-});
+})
 
-export default Sidebar;
+export default Sidebar
