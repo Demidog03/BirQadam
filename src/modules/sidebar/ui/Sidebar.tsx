c5e1e4f98167878d25a5122541f32ce65a8f5e2f
DESKTOP-MTGDTDN\Aleksey <a.n.makarenka@gmail.com>
2024-03-01 22:51:05 +0300

    feat: add changes width

--- src/modules/sidebar/ui/Sidebar.tsx
+++ src/modules/sidebar/ui/Sidebar.tsx
@@ -3,7 +3,7 @@ import { MdDashboard } from 'react-icons/md';
 import usePathnameSegments from '@/shared/lib/hooks/usePathnameSegments.tsx';
 import SidebarMenuItem, { TSidebarMenuItem } from '@/modules/sidebar/ui/SidebarMenuItem.tsx';
 import { useDispatch } from 'react-redux';
-import { setSidebarWidth } from '@/modules/sidebar/model/sidebar.slice.ts';
+import { setSidebarWidth, sidebarSelector } from '@/modules/sidebar/model/sidebar.slice.ts';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/shared/shadcnUI/accordion';
 import SidebarCompanyLogo from './SidebarCompanyLogo';
 import { useSelector } from '@/store';
@@ -45,6 +45,13 @@ const Sidebar = memo(() => {
     const [sidebarMenuItems, setSidebarMenuItems] = useState<TSidebarMenuItem[]>(initialSidebarMenuItems);
     const company = useSelector(companySelector);
     const user = useSelector(profileSelector);
+    const sidebar = useSelector(sidebarSelector);
+
+    const handleClick = () => {
+      if (sidebar?.width) {
+        dispatch(setSidebarWidth(sidebar.width <= 80 ? 260 : 80))
+      }
+    }
 
     useEffect(() => {
       const updatedItems = initialSidebarMenuItems.map(item => ({
@@ -61,9 +68,9 @@ const Sidebar = memo(() => {
     }, [dispatch, sidebarRef]);
 
     return (
-      <Accordion ref={sidebarRef} type="single" collapsible className='relative'>
-        <AccordionItem value="item-1" className='flex flex-col pl-4 pr-4 pt-[20px] pb-[34px] gap-6 border-r border-[#F7F9FC] shadow-[3px_0px_40px_0px_rgba(87,156,216,0.10)] z-10'>
-          <div className='flex mb-8'>
+      <Accordion ref={sidebarRef} type="single" collapsible defaultValue="item-1" className='relative'>
+        <AccordionItem value="item-1" className='flex flex-col min-w-[80px] max-w-[260px] pl-4 pr-4 pt-[20px] pb-[34px] gap-6 shadow-[3px_0px_40px_0px_rgba(87,156,216,0.10)] z-10'>
+          <div className='flex mb-8 cursor-default'>
             <SidebarCompanyLogo imageSrc={company?.logo ? company.logo : ''}/>
             <AccordionContent className='ml-3'>
               <h2 className='text-base font-medium'>{company?.name}</h2>
@@ -75,11 +82,11 @@ const Sidebar = memo(() => {
               key={item.routeName}
               icon={item.icon}
               isActive={item.isActive}
-              text={<AccordionContent>{item.text}</AccordionContent>}
+              text={<AccordionContent className='w-[170px] flex justify-start'>{item.text}</AccordionContent>}
               routeName={item.routeName}
             />
           ))}
-          <AccordionTrigger className='absolute right-0 top-[80px] border border-[#0369a1] rounded-full translate-x-1/2'></AccordionTrigger>
+          <AccordionTrigger onClick={handleClick} className='absolute right-0 top-[80px] border border-[#0369a1] rounded-full translate-x-1/2'></AccordionTrigger>
         </AccordionItem>
       </Accordion>
     );
